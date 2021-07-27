e1a7f921e9c1b3b51d4ac7c700548744157f4888
Hagen Strahl <34143718+hagenstrahl@users.noreply.github.com>
2021-07-27 07:15:04 +0000

    add localization context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from "next/app";
+import { LocalizationProvider } from "../components/shared/localization";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <LocalizationProvider>
+      <Component {...pageProps} />
+    </LocalizationProvider>
+  );
 }
 
 export default MyApp;
