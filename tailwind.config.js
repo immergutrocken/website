139e1672e5fe08f8fe7cad03d208d5e6d91a1df9
Hagen Strahl <hagen.strahl@gmail.com>
2021-12-11 13:45:13 +0100

    upgrade configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grasshopper: "#98CE58",
+      },
+    },
     fontFamily: {
       content: ["Ludwig-Normal"],
       important: ["Ludwig-Bold"],
     },
-    backgroundColor: (theme) => ({
-      ...theme("colors"),
-      grasshopper: "#98CE58",
-    }),
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
